90865d0d9836e3362238e277fd93f83142c03af0
Phayouderson <108438743+Phayouderson@users.noreply.github.com>
2024-04-16 01:23:21 -0400

    add the skill component

--- src/skill.js
+++ src/skill.js
@@ -1,19 +1,35 @@
-import React from 'react'
-import { FaHtml5, FaCss3Alt, FaJava, FaReact } from "react-icons/fa";
+import React from 'react';
+import { FaHtml5, FaCss3Alt, FaReact, FaNodeJs } from 'react-icons/fa';
+import { SiExpress, SiMongodb, SiJavascript } from 'react-icons/si';
+import './skill.css';
 
-const skill = () => {
+const skillsData = [
+  { icon: <FaHtml5 />, name: 'HTML5', className: 'html5' },
+  { icon: <FaCss3Alt />, name: 'CSS3', className: 'css3' },
+  { icon: <SiJavascript />  , name: 'JavaScript', className: 'JavaScript' },
+  { icon: <FaReact />, name: 'React', className: 'react' },
+  { icon: <FaNodeJs />, name: 'Node.js', className: 'nodejs' },
+  { icon: <SiExpress />, name: 'Express', className: 'express' },
+  { icon: <SiMongodb />, name: 'MongoDB', className: 'mongodb' },
+];
+
+const Skill = () => {
   return (
-    <div>
-        <div>
-            <h1>Skill</h1>
-            <FaHtml5 />
-            <FaCss3Alt />
-            <FaJava />
-            <FaReact />
-
-        </div>
+    <div className='skill-container1'>
+      <h1>Skill<span className="textline"></span></h1>
+      <div className="skill-container">
+        {skillsData.map((skill, index) => (
+          <div
+            key={index}
+            className={`skill-item ${skill.className}`}>
+            {skill.icon}
+            <p>{skill.name}</p>
+          </div>
+        ))}
+      </div>
     </div>
-  )
-}
 
-export default skill
+  );
+};
+
+export default Skill;
